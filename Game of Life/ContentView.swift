4ab7af939eb6a71302e00ac144c1c59f2e480980
smuffels <bruncel1@students.zhaw.ch>
2026-02-12 21:02:32 +0100

    make pretty :) and light/darkmode

--- Game of Life/ContentView.swift
+++ Game of Life/ContentView.swift
@@ -10,25 +10,66 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var logic = GameOfLifeLogic()
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State var isDarkmode = false
+    
     var body: some View {
-        if horizontalSizeClass == .regular{
-            HStack{
-                BoardView(label: "start", boardSize: logic.boardSize, boardContent: logic.oldBoard)
-                BoardView(label: "next generation", boardSize: logic.boardSize, boardContent: logic.newBoard)
-            }
-            .padding()}
-        else{
+        ZStack{
+            Color("backgroundColor")
+                .ignoresSafeArea()
             VStack{
-                BoardView(label: "start", boardSize: logic.boardSize, boardContent: logic.oldBoard)
-                    .padding(5)
-                BoardView(label: "next generation", boardSize: logic.boardSize, boardContent: logic.newBoard)
+                ZStack{
+                    Text("Game of Life").textStyle(size: 25)
+                    HStack{
+                        Spacer()
+                        Button {isDarkmode.toggle()}
+                        label: {Image(systemName: isDarkmode ? "sun.max.fill" : "sun.max")
+                                .foregroundStyle(Color("alive"))
+                                .font(.title2)
+                                .padding()
+                        }
+                    }
+                }.padding(.horizontal)
+                //Regular View
+                if horizontalSizeClass == .regular{
+                    HStack{
+                        BoardView(label: "start", boardSize: logic.boardSize, boardContent: logic.oldBoard)
+                        BoardView(label: "next generation", boardSize: logic.boardSize, boardContent: logic.newBoard)
+                    }
+                    .padding()}
+                //Compact View
+                else{
+                    VStack{
+                        BoardView(label: "start", boardSize: logic.boardSize, boardContent: logic.oldBoard)
+                            .padding(5)
+                        BoardView(label: "next generation", boardSize: logic.boardSize, boardContent: logic.newBoard)
+                        Spacer()
+                    }
+                    
+                }
+                
             }
-            Spacer()
-            
+            .preferredColorScheme(isDarkmode ? .dark: .light)
         }
         
     }
 }
+
+struct TextStyle: ViewModifier{
+    var size: Double
+    func body(content: Content) -> some View {
+            content
+            .foregroundStyle(Color("alive"))
+            .font(.system(size: size, weight: .light, design: .serif))
+            .italic()
+        }
+}
+
+extension View{
+    func textStyle(size: Double) -> some View {
+        self.modifier(TextStyle(size: size))
+    }
+}
+
 
 struct BoardView: View {
     var label: String
@@ -36,21 +77,24 @@ struct BoardView: View {
     var boardContent: [[Int]]
     
     var body: some View {
-        GroupBox(label: Text(label).frame(maxWidth: .infinity)){
+        GroupBox(label: Text(label).textStyle(size: 20)
+            .frame(maxWidth: .infinity)){
             VStack(){
                 ForEach(0..<boardSize, id: \.self) { row in
                     HStack(){
                         ForEach(0..<boardSize, id: \.self) { col in
                             Rectangle()
-                                .fill(boardContent[row][col] == 1 ? Color.pink : Color.white)
+                                .fill(boardContent[row][col] == 1 ? Color("alive") : Color("dead"))
                                 .frame(width: 15, height: 15)
-                                .border(Color.gray)
                         }
                     }
                 }
             }
-        } .frame(maxWidth:300)
+        }
+        .backgroundStyle(Color("boxBackground"))
+        .frame(maxWidth:300)
         .aspectRatio(1, contentMode: .fit)
+        
     }
 }
 
